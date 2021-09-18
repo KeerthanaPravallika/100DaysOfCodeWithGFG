d9593d63aed0b66ae4f4415365e392a3860869ec
Keerthana Pravallika Samudrala <66155737+KeerthanaPravallika@users.noreply.github.com>
2021-09-18 15:36:49 +0530

    Create Day4-SumOfQuery2.py

---
+++ Day4-SumOfQuery2.py
@@ -0,0 +1,32 @@
+'''
+Sum of Query II 
+Medium Accuracy: 84.68% Submissions: 2055 Points: 4
+You are given an array arr[] of n integers and q queries in an array queries[] of length 2*q containing l, r pair for all q queries. You need to compute the following sum over q queries.
+
+ 
+
+Array is 1-Indexed.
+
+Example 1:
+
+Input: n = 4
+arr = {1, 2, 3, 4}
+q = 2
+queries = {1, 4, 2, 3}
+Output: 10 5
+Explaination: In the first query we need sum 
+from 1 to 4 which is 1+2+3+4 = 10. In the 
+second query we need sum from 2 to 3 which is 
+2 + 3 = 5.
+'''
+
+class Solution:
+    def querySum(self, n, arr, q, queries):
+        # code here
+        res = []
+        index = 0
+        for i in range(q):
+            rs = sum(arr[queries[index]-1:queries[index+1]])
+            res.append(rs)
+            index += 2
+        return res
